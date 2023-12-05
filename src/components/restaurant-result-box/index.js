222c496ed327af5ab421327af22b56ed40487bba
Masud <ec20503@qmul.ac.uk>
2023-12-05 19:48:18 +0000

    chore: drawer added

--- src/components/restaurant-result-box/index.js
+++ src/components/restaurant-result-box/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import StarIcon from "@mui/icons-material/Star";
+import ViewRestaurant from "../drawer";
+import * as React from "react";
+import Drawer from "@mui/material/Drawer";
 
 const style = {
   position: "absolute",
@@ -24,6 +27,63 @@ const RestaurantResultBox = (props) => {
   const [modal, setModal] = useState(false);
   const [reviews, setReviews] = useState([]);
   const [newReview, setNewReview] = useState(null);
+  const [isViewRestaurant, setIsViewRestaurant] = useState(false);
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? 550 : 550 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <header style={{ border: "2px solid red", minHeight: "2rem" }}>
+        <div >
+          <span className="vote-counter">{upVoteCounter}</span>
+          <Tooltip title="Up Vote the Restaurant" onClick={onUpVoteHandler}>
+            <ThumbUpIcon className="thumb" />
+          </Tooltip>
+          <span className="vote-counter">{downVoteCounter}</span>
+          <Tooltip title="Down Vote the Restaurant" onClick={onDownVoteHandler}>
+            <ThumbDownIcon className="thumb" />
+          </Tooltip>
+        </div>
+      </header>
+      <Box
+        sx={{
+          marginTop: "1rem",
+          border: "2px solid blue",
+          height: "40vh",
+          overflowY: "auto",
+        }}
+      ></Box>
+      <Box
+        sx={{
+          marginTop: "1rem",
+          border: "2px solid blue",
+          height: "45vh",
+          overflowY: "auto",
+        }}
+      ></Box>
+    </Box>
+  );
 
   const fetchReviews = () => {
     fetch("http://localhost:4000/reviews/all")
@@ -89,9 +149,18 @@ const RestaurantResultBox = (props) => {
     "https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC9pbWFnZS1wcm9jL3Byb2Nlc3NlZF9pbWFnZXMvYTQyMjdjZjFlNTJjYjgzZTQzZjE2ZjhjMWNlYWI2ZTAvNzkxNWM0YTc4YTlmOTRlZDU2MzE2YzdjNGRjMGVjODkuanBlZw==";
 
   return (
-    <div className="restaurant-result-box">
+    <div onClick={toggleDrawer("left", true)} className="restaurant-result-box">
       <div className="header">
         {/* <h3>{title}</h3> */}
+
+        <Drawer
+          anchor="left"
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+        >
+          {list("left")}
+        </Drawer>
+
         <div className="voting-thumbs">
           <span className="vote-counter">{upVoteCounter}</span>
           <Tooltip title="Up Vote the Restaurant" onClick={onUpVoteHandler}>
