41989aa218b488af722e8945d2787fa850c0e561
ehtishamdot <ahtionpc1@gmail.com>
2023-12-07 00:16:40 +0500

    auth

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import logo from "./logo.svg";
 import "./App.css";
 import Voting from "./components/voting";
 import Sidebar from "./layout/sidebar";
+import { Routes, Route } from "react-router-dom";
 
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ProtectedRoute from "./ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import Signup from "./components/signup";
+import Login from "./components/login";
 
 
 // UP VOTE AND DOWN VOTE
 function App() {
+
   return (
     <div className="App">
-      <Sidebar />
-      <Voting />
-      
+      {/* <Voting /> */}
+      <UserAuthContextProvider>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Sidebar />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </UserAuthContextProvider>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { createTheme } from '@mui/material/styles';
 
@@ -23,9 +24,11 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme} >
-      <App />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme} >
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
 
